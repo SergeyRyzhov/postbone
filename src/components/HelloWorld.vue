d41db5ce918c5c14dbf363c30b954284167f627f
Sergey Ryzhov <sergey.ryzhov@firstlinesoftware.com>
2020-05-10 10:42:54 +0200

    random post selector

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>
+      <!-- <h1>{{ msg }}</h1> -->
+      <h1>Случайный выбор публикаций</h1>
+    </div>
+    <div>
+      <input placeholder="введите хэштег" v-model="tag" />
+    </div>
+    <div>
+      <button type="button" v-on:click.prevent="selectRandomPost" v-if="!image">Выбрать публикацию</button>
+
+      <button type="button" v-on:click.prevent="resetForm" v-if="!!image">Начать заново</button>
+    </div>
+
+    <div v-if="!!image && !!tag">
+      <a v-bind:href="detailsUrl">
+        <img v-bind:src="image" height="256px" width="256px" />
+      </a>
+      <!-- <p v-text="userName"></p> -->
+    </div>
+
+    <pre>{{tag}}</pre>
   </div>
 </template>
 
 <script>
+let ig = require("instagram-scraping");
+var storage = {};
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data: function() {
+    console.log(ig);
+
+    return {
+      tag: "",
+
+      image: "",
+      userName: "",
+      detailsUrl: ""
+    };
+  },
+  methods: {
+    resetForm() {
+      if(this.tag){
+        this.selectRandomPost();
+      }else{
+        this.$set(this, "image", null);
+        this.$set(this, "tag", null);
+      }
+
+    },
+    selectRandomPost() {
+      // alert(this.tag);
+      if (!this.tag) return;
+
+      if (storage.hasOwnProperty(this.tag)) {
+        startSelection.call(this, storage[this.tag]);
+        return;
+      }
+
+      ig.scrapeTag(this.tag).then(posts => {
+        var reduced = posts.medias.filter(p=> p.owner_id != "4365503338");
+        posts = {
+          total:reduced.length,
+          medias: reduced
+        };
+        storage[this.tag] = posts;
+        startSelection.call(this, posts);
+      });
+    }
   }
+};
+
+function startSelection(posts) {
+  var total = posts.total;
+  var randomIndex = Math.ceil(Math.random() * (total - 1));
+  var timer = setInterval(showRandomPost.bind(this, posts), 200);
+  setTimeout(stopTimer.bind(this, timer), 5000);
+}
+
+function showRandomPost(posts) {
+  this.$nextTick(() => {
+    var total = posts.total;
+    var randomIndex = Math.ceil(Math.random() * (total - 1));
+    var post = posts.medias[randomIndex];
+
+    var thumbnail = post.thumbnail;
+    var image = post.display_url;
+    var user = post.owner_id;
+
+    this.$set(this, "image", thumbnail);
+    this.$set(this, "userName", user);
+    this.$set(this, "detailsUrl", "https://instagram.com/p/" + post.shortcode);
+  });
+}
+
+function stopTimer(timer) {
+  clearInterval(timer);
+
+  /*var thumbnail = post.thumbnail;
+  var image = post.display_url;
+  var user = post.owner_id;
+
+  this.$set(this, "image", image);
+  this.$set(this, "userName", user);*/
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3
-  margin 40px 0 0
+h3 {
+  margin: 40px 0 0;
+}
 
-ul
-  list-style-type none
-  padding 0
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-li
-  display inline-block
-  margin 0 10px
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
 
-a
-  color #42b983
+a {
+  color: #42b983;
+}
 </style>
